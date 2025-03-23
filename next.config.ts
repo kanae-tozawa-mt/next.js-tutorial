bf7c7cc24db344eda7e92a99cb9dc6102669b18c
chocoZap <chocozap@explosion.local>
2025-03-24 01:23:52 +0900

    fix: フォーマットの修正

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-    turbo: true,
-  },
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	experimental: {
+		appDir: true,
+		turbo: true,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
