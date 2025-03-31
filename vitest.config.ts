abb1d0bb947f8c0e8794a8ad7f52454087ed82cf
Yuta-fukui <yuta.fukui@metateam.co.jp>
2025-03-31 16:53:42 +0900

    feat: カバレッジを見れるようにする

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-  },
-}) 
+	plugins: [react()],
+	test: {
+		environment: "jsdom",
+		coverage: {
+			enabled: true, // カバレッジを有効化
+			reporter: ["text", "json", "html"], // 出力形式
+			reportsDirectory: "./coverage", // 保存先ディレクトリ
+			all: true, // 全ファイルのカバレッジを計測
+		},
+	},
+});
