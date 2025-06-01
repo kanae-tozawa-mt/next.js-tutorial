8522116af09b8762e3aa1500fedd28b75d7a2349
kanae-tozawa-mt <kanae.tozawa@metateam.co.jp>
2025-06-01 06:36:00 +0000

    hook修正

--- src/app/hooks.tsx
+++ src/app/hooks.tsx
@@ -1,23 +1,24 @@
-import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { basicFormSchema, BasicFormSchemaType } from "./zod";
+import { useForm } from "react-hook-form";
+import { basicFormSchema } from "./zod";
+import type { BasicFormSchemaType } from "./zod";
 
 export const useBasicFormTop = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<BasicFormSchemaType>({
-    resolver: zodResolver(basicFormSchema),
-  });
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<BasicFormSchemaType>({
+		resolver: zodResolver(basicFormSchema),
+	});
 
-  const onSubmit = (data: BasicFormSchemaType) => {
-    console.log(data);
-  };
+	const onSubmit = (data: BasicFormSchemaType) => {
+		console.log(data);
+	};
 
-  return {
-    register,
-    onSubmit: handleSubmit(onSubmit),
-    errors,
-  };
+	return {
+		register,
+		onSubmit: handleSubmit(onSubmit),
+		errors,
+	};
 };
